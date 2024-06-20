6671287e126ad6653bb3b6b5add6860ea464a0b9
chanoi8558 <oiss8558ssoi@gmail.com>
2024-06-20 15:51:22 +0900

    Userモデルにアソシエーションと各カラムのバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :purchases
+        
+  validates :nickname, presence: true
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, presence: true
+  validates :birthdate, presence: true
 end
