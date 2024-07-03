da9a7122991e746bb2034289817b291c901d09b8
chanoi8558 <oiss8558ssoi@gmail.com>
2024-07-03 18:41:07 +0900

    新規登録のユーザー情報、本人情報確認、トップページ実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,18 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :purchases
-        
+
+   # ニックネームが必須
   validates :nickname, presence: true
-  validates :last_name, :first_name, :last_name_kana, :first_name_kana, presence: true
+
+   # 名前は全角（漢字、カナ、かな）で、名字と名前が必須。生年月日が必須
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  VALID_KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :last_name, :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: VALID_KANA_NAME_REGEX }
   validates :birthdate, presence: true
+
+  # パスワードは半角英数字混合
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'は半角英字と数字の両方を含めて設定してください'
 end
