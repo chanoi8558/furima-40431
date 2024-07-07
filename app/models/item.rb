ea5e60db14d30a0ab6ee24532f7212d125dcb7aa
chanoi8558 <oiss8558ssoi@gmail.com>
2024-07-07 21:49:56 +0900

    商品モデル、バリデーション、activestorage設定、コントローラー、ルーティング設定

---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_time_id, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+end
