be6467e16162f3df4351296e529513cbf10729b5
chanoi8558 <oiss8558ssoi@gmail.com>
2024-07-10 21:34:16 +0900

    コードレビュー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 0 , message: "can't be blank" } # assuming 0 is the blank value for prefectures
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_time_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image, presence: true
