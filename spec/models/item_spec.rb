4ab6f89bfe93827aa0a1e994700dd404a386ed9f
chanoi8558 <oiss8558ssoi@gmail.com>
2024-07-10 21:13:46 +0900

    コードレビュー修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe Item, type: :model do
       end
 
       it 'カテゴリーの情報が空では出品できない' do
-        @item.category_id = ''
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
